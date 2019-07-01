25402946ce3c345133189c6998edc97f90d07532
matatsuna <matatsuna@gmail.com>
2019-07-01 20:37:03 +0900

    add snapsvg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hogehuga");
+const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
+
+document.addEventListener("DOMContentLoaded", (() => {
+    var s = Snap("#svg");
+    var bigCircle = s.circle(150, 150, 100);
+    bigCircle.attr({
+        fill: "#bada55",
+        stroke: "#000",
+        strokeWidth: 5
+    });
+}));
